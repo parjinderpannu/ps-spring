d426eab177a69ad623f8081f29fb08296b06da67
Parjinder Pannu <ppannu@cisco.com>
2018-02-03 21:02:31 -0800

    properties example

--- src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -6,12 +6,20 @@ import java.util.List;
 import com.pluralsight.model.Customer;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
+	
+	private String dbUsername;
+
+	public void setDbUsername(String dbUsername) {
+		this.dbUsername = dbUsername;
+	}
 
 	/* (non-Javadoc)
 	 * @see com.pluralsight.repository.CustomerRepository#findAll()
 	 */
 	@Override
 	public List<Customer> findAll() {
+		System.out.println(dbUsername);
+		
 		List<Customer> customers = new ArrayList<>();
 		
 		Customer customer = new Customer();
