9892aeb73b5a0cfb5225843080ff9a504a2ea593
Parjinder Pannu <ppannu@cisco.com>
2018-02-03 21:05:32 -0800

    anno properties

--- src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -3,15 +3,15 @@ package com.pluralsight.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import com.pluralsight.model.Customer;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 	
+	@Value("${dbUsername}")
 	private String dbUsername;
 
-	public void setDbUsername(String dbUsername) {
-		this.dbUsername = dbUsername;
-	}
 
 	/* (non-Javadoc)
 	 * @see com.pluralsight.repository.CustomerRepository#findAll()
